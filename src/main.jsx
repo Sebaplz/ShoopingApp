eeb34d80a5ee45b5b1ca73a013432d19d370a225
Sebaplz <sebastian.h.neira@outlook.com>
2023-10-22 21:56:58 -0300

    Se cambia a HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { FiltersProvider } from "./context/Filters.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <FiltersProvider>
       <App />
     </FiltersProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
